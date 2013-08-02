cba2d0217e8ee11396023c00a235a8301d7d0065
Simon Schmidt <sfs@marketing-factory.de>
2013-08-02 12:44:50 +0000

    add error mail if redirect doesn't work

--- realurl_hook/class.tx_realurl_hooksHandler.php
+++ realurl_hook/class.tx_realurl_hooksHandler.php
@@ -1,6 +1,7 @@
 <?php
-require_once(t3lib_extMgm::extPath('restructure_redirect').'class.tx_restructure_linkcreator.php');
-class tx_realurl_hooksHandler  {
+require_once(t3lib_extMgm::extPath('restructure_redirect') . 'class.tx_restructure_linkcreator.php');
+class tx_realurl_hooksHandler
+{
 
 	/**
 	 *
@@ -11,16 +12,17 @@ class tx_realurl_hooksHandler  {
 	 * @param object $pObj
 	 *
 	 */
-	function user_decodeSpURL_preProc ( $hookParams, $pObj ) {
-		if (TYPO3_MODE =='BE') {
+	function user_decodeSpURL_preProc($hookParams, $pObj)
+	{
+		if (TYPO3_MODE == 'BE') {
 			return;
 		}
 		$table = "tx_restructureredirect_redirects";
 		$enableFields = "
 		AND tx_restructureredirect_redirects.deleted=0
 		AND tx_restructureredirect_redirects.hidden=0
-		AND tx_restructureredirect_redirects.starttime<=".time()."
-		AND (tx_restructureredirect_redirects.endtime=0 OR tx_restructureredirect_redirects.endtime>".time().")
+		AND tx_restructureredirect_redirects.starttime<=" . time() . "
+		AND (tx_restructureredirect_redirects.endtime=0 OR tx_restructureredirect_redirects.endtime>" . time() . ")
 		AND (
 			tx_restructureredirect_redirects.fe_group=''
 			OR tx_restructureredirect_redirects.fe_group IS NULL
@@ -29,25 +31,26 @@ class tx_realurl_hooksHandler  {
 			OR FIND_IN_SET('-1',tx_restructureredirect_redirects.fe_group
 		))";
 
-		$where = "(url='" . $GLOBALS['TYPO3_DB']->quoteStr($hookParams['URL'],$table)."'";
-		$where .= "OR url='/" . $GLOBALS['TYPO3_DB']->quoteStr($hookParams['URL'],$table)."')";
-		$where .= ' AND (expire=0 OR  expire>'.time().') ';
-		$res = $GLOBALS['TYPO3_DB']->exec_SELECTQuery('*', $table,$where . $enableFields, '','',1);
+		$where = "(url='" . $GLOBALS['TYPO3_DB']->quoteStr($hookParams['URL'], $table) . "'";
+		$where .= "OR url='/" . $GLOBALS['TYPO3_DB']->quoteStr($hookParams['URL'], $table) . "')";
+		$where .= ' AND (expire=0 OR  expire>' . time() . ') ';
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTQuery('*', $table, $where . $enableFields, '', '', 1);
 
 		if ($res) {
 			$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
-			if ($row['url'] && strlen($row['url'])> 0) {
+			if ($row['url'] && strlen($row['url']) > 0) {
 				$redirectId = $row['pid'];
 				$params = $this->getUrlParams($hookParams[URL]);
-				$domain = t3lib_befunc::getViewDomain($redirectId).'/';
+				$domain = t3lib_befunc::getViewDomain($redirectId) . '/';
 				//$domain = 'http://www.heimwerker.de/';
 				unset ($params['id']);
-				$its_link = t3lib_div::makeInstance('tx_restructure_linkcreator',$redirectId);
-				$redirectUrl = $its_link->getLink($redirectId,$params);
-				if ($redirectUrl == $hookParams[URL]  ) {
+				$its_link = t3lib_div::makeInstance('tx_restructure_linkcreator', $redirectId);
+				$redirectUrl = $its_link->getLink($redirectId, $params);
+				if ($redirectUrl == $hookParams[URL]) {
+					$this->sendErrorMail($redirectUrl);
 					return;
 				}
-				$redirectUrl = $domain.$redirectUrl;
+				$redirectUrl = $domain . $redirectUrl;
 				t3lib_utility_Http::redirect(t3lib_div::locationHeaderUrl($redirectUrl), t3lib_utility_Http::HTTP_STATUS_301);
 			}
 		}
@@ -61,13 +64,14 @@ class tx_realurl_hooksHandler  {
 	 *
 	 */
 
-	private function getUrlParams ($url) {
+	private function getUrlParams($url)
+	{
 		$table = 'tx_realurl_urlencodecache';
-		$where = "content ='" .$url."'";
-		$res = $GLOBALS['TYPO3_DB']->exec_SELECTQuery('*', $table,$where , '','',1);
+		$where = "content ='" . $url . "'";
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTQuery('*', $table, $where, '', '', 1);
 		if ($res) {
 			$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
-			$origparams  = t3lib_div::trimExplode('|', $row['origparams']);
+			$origparams = t3lib_div::trimExplode('|', $row['origparams']);
 			if ($origparams[1]) {
 				$params = t3lib_div::explodeUrl2Array($origparams[1]);
 				return $params;
@@ -76,5 +80,17 @@ class tx_realurl_hooksHandler  {
 		return array();
 	}
 
+	/**
+	 * send error mail with url, if redirect doesn't work because url and redirect url are the same
+	 * @param $foundUrl string
+	 */
+	private function sendErrorMail($foundUrl) {
+		$subject = 'problems on host ' . $GLOBALS['_ENV']['HTTP_HOST'] .' with url ' . $foundUrl;
+
+		mail('kontroll.heimwerker@marketing-factory.de', $subject, 'Please test the url ' . $GLOBALS['TSFE']->config['config']['baseURL'] . $foundUrl . ' on host ' . $GLOBALS['_ENV']['HTTP_HOST'] );
+
+	}
+
 }
+
 ?>
