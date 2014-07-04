e30a67ae2fd22c12256aabb06b381249877eb76b
Simon Schmidt <sfs@marketing-factory.de>
2014-07-04 11:56:56 +0000

    fix problem with page can't be moved todo 23485

--- class.tx_restructure_linkcreator.php
+++ class.tx_restructure_linkcreator.php
@@ -1,30 +1,24 @@
 <?php
-require_once(PATH_tslib.'class.tslib_fe.php');
-require_once(PATH_t3lib.'class.t3lib_userauth.php');
-require_once(PATH_tslib.'class.tslib_feuserauth.php');
-require_once(PATH_t3lib.'class.t3lib_cs.php');
-require_once(PATH_tslib.'class.tslib_content.php');
-require_once(PATH_t3lib.'class.t3lib_tstemplate.php');
-
-
 /**
  * class for creating fe link from be
  *
  *
  */
 
+/**
+ * Class tx_restructure_linkcreator
+ */
 class tx_restructure_linkcreator {
 	protected $localTSFE;
 
-	function __construct($uid) {
-		$TSFEclassName = t3lib_div::makeInstance('tslib_fe');
+	public function __construct($uid) {
 		$rootline = t3lib_BEfunc::BEgetRootLine($uid);
 		$rootpid = 1;
 		foreach ($rootline as $line ) {
-			if ($line[uid] > 0) {
-				$rootpid = $line[uid];
+			if ($line['uid'] > 0) {
+				$rootpid = $line['uid'];
 			}
-			if ($line[is_siteroot] == 1 ) {
+			if ($line['is_siteroot'] == 1 ) {
 				break;
 			}
 		}
@@ -34,8 +28,9 @@ class tx_restructure_linkcreator {
 			$GLOBALS['TT']->start();
 		}
 	// Create the TSFE class.
-
+		/** @var tslib_fe localTSFE */
 		$this->localTSFE = t3lib_div::makeInstance('tslib_fe',$GLOBALS['TYPO3_CONF_VARS'],$rootpid,'0',1,'','','','');
+		$GLOBALS['TSFE'] = $this->localTSFE;
 		$this->localTSFE->connectToDB();
 		$this->localTSFE->initFEuser();
 		$this->localTSFE->fetch_the_id();
@@ -44,7 +39,6 @@ class tx_restructure_linkcreator {
 		$this->localTSFE->tmpl->getFileName_backPath = PATH_site;
 		$this->localTSFE->forceTemplateParsing = 1;
 		$this->localTSFE->getConfigArray();
-		$GLOBALS['TSFE'] = $this->localTSFE;
 		$GLOBALS['TSFE']->includeTCA();
 	}
 
@@ -80,7 +74,7 @@ class tx_restructure_linkcreator {
 				'tstamp' => time(),
 				'crdate' => time()
 			);
-			$res=$GLOBALS['TYPO3_DB']->exec_INSERTquery( $table , $field_values );
+			$GLOBALS['TYPO3_DB']->exec_INSERTquery( $table , $field_values );
 		}
 	}
 
@@ -91,10 +85,10 @@ class tx_restructure_linkcreator {
 	 * @return boolean
 	 *
 	 */
-	private function linkExists ($link) {
+	protected function linkExists ($link) {
 
 		$table = "tx_restructureredirect_redirects";
-		$enableFields =   t3lib_BEfunc::BEenableFields($table);
+		$enableFields = t3lib_BEfunc::BEenableFields($table);
 		$where = "url=". $GLOBALS['TYPO3_DB']->fullQuoteStr($link,$table);
 		$res = $GLOBALS['TYPO3_DB']->exec_SELECTQuery('*', $table,$where . $enableFields);
 		if ($res) {
